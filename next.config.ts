e62ad176609ecac7458b54fffb4a61408211993c
ahmedhamohamed11-hub <ahmedha.mohamed11@gmail.com>
2026-06-14 21:27:49 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,11 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    skipWaiting: true,
+    clientsClaim: true,
+  },
 });
 
 const nextConfig: NextConfig = {
