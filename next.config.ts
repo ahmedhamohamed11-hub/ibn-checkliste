cc9cbe2b75651f42d29bdd188da2d54e7b16337a
ahmedhamohamed11-hub <ahmedha.mohamed11@gmail.com>
2026-06-14 21:09:58 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: "standalone",
   experimental: {
-    optimizePackageImports: ['lucide-react'],
+    optimizePackageImports: ["lucide-react"],
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
